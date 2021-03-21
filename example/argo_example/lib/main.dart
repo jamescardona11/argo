77647a5fb06683f60504a74b2a5b36994666c208
jamescardona11 <jjcardonao@gmail.com>
2021-03-21 10:41:18 -0500

    Init argo package, the main goal is create a responsive widgets and tools for cross-platforms

---
+++ example/argo_example/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:argo/argo.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
