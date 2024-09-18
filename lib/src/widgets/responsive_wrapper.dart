dcc84c4bbd59317ae9e7e5a50e27152eb224188f
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-09-17 20:48:33 -0500

    Update responsive wrapper

--- lib/src/widgets/responsive_wrapper.dart
+++ lib/src/widgets/responsive_wrapper.dart
@@ -1,8 +1,4 @@
 import 'package:argo/src/models/models.dart';
-import 'package:argo/src/theme/default_theme_data.dart';
-import 'package:argo/src/theme/i_theme_data_rule.dart';
-import 'package:argo/src/theme/responsive_theme.dart';
-import 'package:argo/src/utils/utils.dart';
 import 'package:flutter/material.dart';
 
 /// {@template responsive_wrapper}
@@ -10,131 +6,57 @@ import 'package:flutter/material.dart';
 /// Widget to configure the responsive for the application.
 /// You can use it like global widget or builder widget into `MaterialApp` Widget.
 /// Additionally allows to config the [WrapperConfig].
-/// You can make the manage for `Responsive Theme`
 ///
-/// This widget uses `InheritedWidget` to access from the app to the `wrapConfig`
+/// This widget uses `InheritedWidget` to access from the app to the `globalBreakpoints`
 ///
 /// The basic use:
 /// ``` dart
 /// MaterialApp(
 ///  home: ResponsiveWrapper(
-///       wrapConfig: WrapperConfig(
-///         globalBreakpoints: ScreenBreakpoints(
-///           mobile: 321,
-///           tablet: 650,
+///       globalBreakpoints: ScreenBreakpoints(
+///         mobile: 321,
+///         tablet: 650,
 ///         desktop: 1100,
-///         ),
 ///       ),
 ///       child: Home(),
 ///    ),
 /// )
 /// ```
-/// Also [ResponsiveWrapper] can be the parent of `MaterialApp`
-///
-/// To config `Responsive Theme` use the `responsiveTheme`
-///
 ///
 /// {@endtemplate}
 
 class ResponsiveWrapper extends StatelessWidget {
   const ResponsiveWrapper({
     super.key,
-    required Widget child,
-    this.responsiveTheme,
-    this.wrapConfig = const WrapperConfig(),
-    // this.debugLogDiagnostics = false,
-  }) : child = child;
-
-  const ResponsiveWrapper.builder({
-    super.key,
-    required RWBuilder builder,
-    this.responsiveTheme,
-    this.wrapConfig = const WrapperConfig(),
-    // this.debugLogDiagnostics = false,
-  }) : child = builder;
+    required this.child,
+    this.globalBreakpoints = defaultMinimumBreakPoints,
+  });
 
   /// The widget child
-  final dynamic child;
+  final Widget child;
 
-  /// Class to manage and handle the app themes
-  final ResponsiveTheme? responsiveTheme;
-
-  /// The global configuration of `ResponsiveWrapper`
-  final WrapperConfig wrapConfig;
-
-  /// debug logs
-  // final bool debugLogDiagnostics;
+  /// Global screen breakpoints for the app
+  final ScreenBreakpoints globalBreakpoints;
 
   @override
   Widget build(BuildContext context) {
-    final IThemeDataRule themeData = getThemeDataFromCondition(context);
-
     return _IWResponsiveWrapper(
-      wrapConfig: wrapConfig,
-      child: returnValue(child, themeData),
+      globalBreakpoints: globalBreakpoints,
+      child: child,
     );
   }
 
-  bool _isRBuilder(dynamic data) => data is RWBuilder;
-
-  Widget returnValue(dynamic data, IThemeDataRule themeData) {
-    if (_isRBuilder(data)) {
-      return (data as RWBuilder)(themeData, wrapConfig.themeRule);
-    }
-
-    return responsiveTheme != null
-        ? Theme(
-            data: themeData.getThemeByRule(wrapConfig.themeRule),
-            child: data as Widget,
-          )
-        : data as Widget;
-  }
-
-  IThemeDataRule getThemeDataFromCondition(BuildContext context) {
-    IThemeDataRule themeData = const DefaultThemeDataRule();
-
-    if (responsiveTheme != null) {
-      if (responsiveTheme!.type == ConditionType.conditions) {
-        themeData = ArgoUtils.valueFromConditionByBreakpoints<IThemeDataRule>(
-          context: context,
-          condition: responsiveTheme!.changeWhen!,
-          localBreakpoints: wrapConfig.globalBreakpoints,
-          defaultValue: responsiveTheme!.defaultTheme ?? themeData,
-        )!;
-      } else {
-        themeData = ArgoUtils.valueFromConditionByScreen<IThemeDataRule>(
-          context: context,
-          condition: responsiveTheme!.conditionScreen!,
-          localBreakpoints: wrapConfig.globalBreakpoints,
-          defaultValue: responsiveTheme!.defaultTheme ?? themeData,
-        )!;
-      }
-    }
-
-    return themeData;
-  }
-
-  static WrapperConfig getWrapperConfig(BuildContext context) => _IWResponsiveWrapper.of(context)?.wrapConfig ?? const WrapperConfig();
-
-  static bool isMobile(BuildContext context) => _getDeviceScreen(context).isMobile();
-
-  static bool isTablet(BuildContext context) => _getDeviceScreen(context).isTablet();
-
-  static bool isDesktop(BuildContext context) => _getDeviceScreen(context).isDesktop();
-
-  static DeviceScreen _getDeviceScreen(BuildContext context) {
-    final rw = getWrapperConfig(context);
-    return DeviceScreenX.fromBreakpoint(rw.getDeviceWidth(MediaQuery.of(context).size), rw.globalBreakpoints);
-  }
+  static ScreenBreakpoints getGlobalBreakpoints(BuildContext context) =>
+      _IWResponsiveWrapper.of(context)?.globalBreakpoints ?? defaultMinimumBreakPoints;
 }
 
 @immutable
 class _IWResponsiveWrapper extends InheritedWidget {
-  final WrapperConfig wrapConfig;
+  final ScreenBreakpoints globalBreakpoints;
 
   const _IWResponsiveWrapper({
     required super.child,
-    required this.wrapConfig,
+    required this.globalBreakpoints,
   });
 
   static _IWResponsiveWrapper? of(BuildContext context) => context.dependOnInheritedWidgetOfExactType<_IWResponsiveWrapper>();
